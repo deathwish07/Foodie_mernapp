067a3b8cd2bd2095ebacd1b7d58fbbd708d163b1
deathwish07 <ayanaratharva@gmail.com>
2023-07-06 12:24:59 +0530

    second commit

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
 
-const mongoURI = 'mongodb+srv://deathwish07:Athuu007@clster0.ip6panv.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://deathwish07:Athuu007@clster0.ip6panv.mongodb.net/foodie_mern?retryWrites=true&w=majority'
 const mongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         if (err) console.log("---", err)
         else {
             console.log("Connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function (err, data) {
+                if (err) console.log(err);
+                else console.log();
+            })
         }
     })
 }
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const mongoDB = require("./db")
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoDB = require("./db");
 mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
+});
+app.use('/api', require("./routes/CreateUser"));
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}); 
